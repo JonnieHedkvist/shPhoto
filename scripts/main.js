20d815149c73351d829328c13f42bf96d1678cfb
Jonnie Hedkvist <jonniehedkvist@jonnies-mbp-2.lan>
2014-10-14 17:03:51 +0200

    new fonts and content

--- scripts/main.js
+++ scripts/main.js
@@ -4,19 +4,31 @@ var app = angular.module('shPhoto', []);
 app.controller('MenuController', function($scope, $http){
 	$scope.categories = [
 		{
-			header: 'People'
+			header: 'Portfolio'
 		},
 		{
-			header: 'Nature'
+			header: 'About'
 		},
 		{
-			header: 'Buildings'
-		},
-		{
-			header: 'Music'
+			header: 'Contact'
 		}
 	];
 	//$http.get('data/categories.json').success(function(data) {
 	//    $scope.categories = data;
 	//});
+});
+
+app.controller('ImageController', function($scope){
+	$scope.images = [
+		{
+			src: 'http://lorempixel.com/640/400/people/'
+		},
+		{
+			src: 'http://lorempixel.com/640/400/people/'
+		},
+		{
+			src: 'http://lorempixel.com/640/400/people/'
+		}
+	];
+
 });
